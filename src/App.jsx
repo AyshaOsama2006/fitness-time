1566c3d4e477ba49e46c4a6433f9b40f83778549
tabarak <tabarakzitawi@gmail.com>
2026-03-21 07:15:48 +0200

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Login from './Pages/Login/Login'
 import Nutrition from './Pages/Nutrition/Nutrition'
-
+import TrainersPage from './Pages/Trainers/TrainerCard'
+//import Nanber from './Components/Nanber'
 export default function App() {
+ 
   return (
+    <>
+  {/* <Nanber/> */}
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/nutrition" element={<Nutrition />} />
+        <Route path="/TrainersPage" element={<TrainersPage />} />
+
       </Routes>
     </BrowserRouter>
-  )
+    
+  </>
+   );
 }
