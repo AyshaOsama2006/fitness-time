7fb4081feb5c83d1abe663c194e3f152aa67e060
tabarak-z <tabarakzitawi@gmail.com>
2026-03-23 02:36:55 +0200

    Remove TrainersPage route from App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Login from './Pages/Login/Login'
 import Nutrition from './Pages/Nutrition/Nutrition'
-import TrainersPage from './Pages/Trainers/TrainerCard'
+
 //import Nanber from './Components/Nanber'
 export default function App() {
  
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/nutrition" element={<Nutrition />} />
-        <Route path="/TrainersPage" element={<TrainersPage />} />
+    
 
       </Routes>
     </BrowserRouter>
