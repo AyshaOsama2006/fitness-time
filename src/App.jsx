4caca15af644b8b4dfac84eb47d940638c39a60f
Obayda-Amer <s12429584@stu.najah.edu>
2026-03-27 22:46:32 +0300

    FunalUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import Nutrition from './Pages/Nutrition/Nutrition'
 import Profile from './Pages/Profile/Profile'
+import Store from './Pages/Store/Store'
+import Header from './Components/Header/Header'
 export default function App() {
   return (
     <BrowserRouter>
+     <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/store" element={<Store />} />
         <Route path="/login" element={<Login />} />
         <Route path="/nutrition" element={<Nutrition />} />
-                <Route path="/Profile" element={<Profile />} />
-
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
