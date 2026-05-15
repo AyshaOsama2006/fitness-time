d09961d20aa86b90c582e7334267faea020fcda5
somabeleh <s12429370@stu.najah.edu>
2026-05-15 17:03:14 +0300

    update frontend

--- src/Components/Membership/PlanCard.jsx
+++ src/Components/Membership/PlanCard.jsx
@@ -1,7 +1,106 @@
-import React from "react";
-import '../../Pages/Membership/Membership.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "../../Pages/Membership/Membership.css";
 
-function PlanCard({ image, title, price, period, save, features, highlight }) {
+function PlanCard({
+  image,
+  title,
+  price,
+  period,
+  save,
+  features,
+  highlight,
+  id
+}) {
+
+  const handleChoose = async () => {
+
+    console.log("BUTTON CLICKED");
+
+    try {
+
+      console.log("SENDING REQUEST");
+let statusValue = "active";
+let endDate = "";
+
+if (title === "MONTHLY") {
+  endDate = "2026-06-14";
+}
+
+else if (title === "ANNUAL") {
+  endDate = "2027-05-14";
+}
+
+else if (title === "VIP") {
+  endDate = "2026-06-14";
+  statusValue = "vip-active";
+}
+      const res = await axios.post(
+        "http://localhost:5000/api/subscriptions",
+        {
+          userId: 1,
+          membershipId: id,
+           start_date: "2026-05-14",
+    end_date: endDate,
+    status: statusValue
+  }
+        
+      );
+
+      console.log("SUCCESS:", res.data);
+setSubscriptionId(res.data.id);
+console.log("NEW SUB ID:", res.data.id);
+      alert("Selected!");
+
+    } catch (err) {
+
+      console.log("FULL ERROR:", err);
+
+      if (err.response) {
+        console.log("SERVER ERROR:", err.response.data);
+        alert(JSON.stringify(err.response.data));
+      } else {
+        console.log("NO RESPONSE FROM SERVER");
+      }
+
+      alert(" Error");
+    }
+  };
+const [subscriptionId, setSubscriptionId] = useState(null);
+  const handleDelete = async () => {
+
+  console.log("DELETE BUTTON CLICKED");
+
+  try {
+
+    console.log("SENDING DELETE REQUEST");
+
+    const res = await axios.delete(
+      `http://localhost:5000/api/subscriptions/${subscriptionId}`
+    );
+
+    console.log("DELETE SUCCESS:", res.data);
+
+    alert("Plan Deleted!");
+
+  } catch (err) {
+
+    console.log("FULL DELETE ERROR:", err);
+
+    if (err.response) {
+
+      console.log("SERVER ERROR:", err.response.data);
+
+      alert(JSON.stringify(err.response.data));
+
+    } else {
+
+      console.log("NO RESPONSE FROM SERVER");
+
+      alert("Delete Error");
+    }
+  }
+};
   return (
     <div className={`plan-card ${highlight ? "active" : ""}`}>
 
@@ -21,19 +120,30 @@ function PlanCard({ image, title, price, period, save, features, highlight }) {
 
         <ul className="features">
           {features.map((f, index) => (
-            <li key={index} className={!f.available ? "disabled" : ""}>
+            <li
+              key={index}
+              className={!f.available ? "disabled" : ""}
+            >
               <span className={f.available ? "check" : "cross"}>
                 {f.available ? "✔" : "✖"}
               </span>
+
               {f.text}
             </li>
           ))}
         </ul>
 
-        <button className="btn-plan">CHOOSE PLAN</button>
+        <button className="btn-plan" onClick={handleChoose}>
+          CHOOSE PLAN
+        </button>
+      <button
+  className="btn-delete"
+  onClick={handleDelete}
+>
+  DELETE PLAN
+</button>
 
       </div>
-
     </div>
   );
 }
